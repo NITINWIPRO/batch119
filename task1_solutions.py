7871881fdac20b533f6eaf939d359b2a52694d14
Ayushi Jain <124241088+Ayushi0214@users.noreply.github.com>
2023-03-28 19:54:31 +0530

    Create task1_solutions.py

---
+++ task1_solutions.py
@@ -0,0 +1,30 @@
+
+Tasks:
+1. write a program to find the area of a circle, take the input from the user for the radius.
+
+radius = float(input('enter radius: '))
+area = ((22/7)*(radius**2))
+print(area)
+
+
+
+
+2. Imagine you are working at a college, create a form where you ask about different details from the user in order to create their id card. 
+
+name = input("enter name:")
+age = int(input("enter age: "))
+gender = input("enter gender: ")
+year = input("enter semester: ")
+ph_no = int(input("enter phone number: "))
+
+
+3. write a program to find the cube of 36. 
+4. Apply all the types of Arithmetic Operators on the following Variables:
+    a = 40
+    b = 5
+    
+5. x = 56
+   y = "44"
+   
+   write a program to display sum of both the variables. 
+   and the output of the program should be 100.
